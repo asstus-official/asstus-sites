9e81b6702700528025f3aedabdc4e5ff848f6812
asstus <asstus.com+g1thub@gmail.com>
2025-09-27 13:42:28 +0000

    phucpt: website 1st release

--- src/docusaurus.config.ts
+++ src/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://asstus.dev.pages',
+  url: 'https://asstus.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
